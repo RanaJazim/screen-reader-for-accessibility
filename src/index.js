b3b2665db78f9e3e87c4ede5ce4aeb6c7c402de3
RanaJazim <stackdeveloper87@gmail.com>
2021-05-25 19:38:55 -0700

    added screen reader with some of functionallity ..
    functionallity includes i.e. speakable content, traversing the dom, highlight the current speakable content by blue border ...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("screen reader");
+import $ from "jquery";
+import ScreenReader from "./utils/screen-reader";
+
+function runApp() {
+  const reader = ScreenReader();
+
+  $(document).keydown((e) => {
+    if (reader.isReaderEnabled()) {
+      if (e.code === "ArrowRight") reader.readFromNext();
+      else if (e.code === "ArrowLeft") reader.readFromPrev();
+      else if (e.code === "KeyS") reader.stop();
+    } else {
+      if (e.code === "KeyR" && !e.ctrlKey) reader.readFromStart();
+    }
+  });
+}
+
+console.log(document.querySelectorAll("*"));
+
+runApp();

76b6947d78c38f64f9b0e471e160164a567f8fa3
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-22 13:33:24 +0500

    using promises for speech synthesis api instead using callbacks ..

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import $ from "jquery";
-import ScreenReader from "./utils/screen-reader";
+// import ScreenReader from "./utils/screen-reader";
 
+// function runApp() {
+//   const reader = ScreenReader();
+
+//   $(document).keydown((e) => {
+//     if (reader.isReaderEnabled()) {
+//       if (e.code === "ArrowRight") reader.readFromNext();
+//       else if (e.code === "ArrowLeft") reader.readFromPrev();
+//       else if (e.code === "KeyS") reader.stop();
+//     } else {
+//       if (e.code === "KeyR" && !e.ctrlKey) reader.readFromStart();
+//     }
+//   });
+// }
+
+// runApp();
+
+// custom code here
 function runApp() {
-  const reader = ScreenReader();
+  console.log("say");
 
-  $(document).keydown((e) => {
-    if (reader.isReaderEnabled()) {
-      if (e.code === "ArrowRight") reader.readFromNext();
-      else if (e.code === "ArrowLeft") reader.readFromPrev();
-      else if (e.code === "KeyS") reader.stop();
-    } else {
-      if (e.code === "KeyR" && !e.ctrlKey) reader.readFromStart();
+  const msgs = ["first message", "second message", "third message"];
+
+  $(document).keydown(async (e) => {
+    if (e.code === "KeyR") {
+      for (let i = 0; i < msgs.length; i++) {
+        await speak(msgs[i]);
+      }
     }
+  });
+}
+
+function speak(message) {
+  const utterance = new SpeechSynthesisUtterance(message);
+  speechSynthesis.speak(utterance);
+  return new Promise((resolve) => {
+    const id = setInterval(() => {
+      if (speechSynthesis.speaking === false) {
+        clearInterval(id);
+        resolve();
+      }
+    }, 100);
   });
 }
 

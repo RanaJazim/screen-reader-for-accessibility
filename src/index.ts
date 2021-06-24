942418e9ea77c4b48840c712648ca4838be29d2b
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-25 04:43:27 +0500

    renamed custom element to accessibility-tool ..

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { CustomElement } from "../custom-element";
 import { appBootstrap } from "./utils/app-bootstrap";
 
 function main() {
-  customElements.define("custom-element", CustomElement);
+  customElements.define("accessibility-tool", CustomElement);
 
   appBootstrap();
 }

a51354a25cbc2d67366ad6299dbb3260ca748634
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-24 10:49:23 +0500

    added enum for checking keys to read the content of webpage like what key to stop, start etc ..

--- src/utils/read-web-content.ts
+++ src/utils/read-web-content.ts
@@ -1,13 +1,18 @@
 import { ScreenReader } from "./screen-reader";
+import { ReaderKeys } from "./reader-keys";
 
 export function readWebPageContent(reader: ScreenReader) {
   return (event: KeyboardEvent) => {
     if (reader.isReaderEnabled) {
-      if (event.code === "ArrowRight") reader.jumpToNext();
-      else if (event.code === "ArrowLeft") reader.jumpToPrevious();
-      else if (event.code === "KeyS") reader.stopReading();
+      if (event.code === ReaderKeys.NEXT) reader.jumpToNext();
+      else if (event.code === ReaderKeys.PREV) reader.jumpToPrevious();
+      else if (event.code === ReaderKeys.STOP) reader.stopReading();
     } else {
-      if (event.code === "KeyR" && !event.ctrlKey && !reader.isReaderEnabled) {
+      if (
+        event.code === ReaderKeys.START &&
+        !event.ctrlKey &&
+        !reader.isReaderEnabled
+      ) {
         reader.startReading();
       }
     }
---
+++ src/utils/reader-keys.ts
@@ -0,0 +1,6 @@
+export enum ReaderKeys {
+  START = "KeyR",
+  STOP = "KeyS",
+  NEXT = "ArrowRight",
+  PREV = "ArrowLeft",
+}
